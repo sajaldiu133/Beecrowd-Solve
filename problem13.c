f031f5c3cb6d6a9dc334ebc1ba3369b8a3b2b034
sajaldiu133 <sajaldiu133@gmail.com>
2026-01-01 23:10:33 +0600

    beeceowd-1005

---
+++ problem13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+double A, B, media;
+
+scanf("%lf %lf", &A, &B);
+
+media = (A * 3.5 + B * 7.5)/(3.5+11.5);
+
+printf("MEDIA = %.5lf\n", media);
+
+
+
+return 0;
+}
