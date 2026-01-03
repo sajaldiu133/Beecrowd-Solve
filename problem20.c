e210d9caad1f47cc38274cbe95dc77773d0239d4
sajaldiu133 <sajaldiu133@gmail.com>
2026-01-03 23:03:28 +0600

    beecrowd-1036 - Bhaskara's Formula

---
+++ problem20.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double A, B, C;
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    if (A == 0)
+    {
+        printf("Impossivel calcular\n");
+    }
+    else if ((B*B - 4*A*C) < 0)
+    {
+        printf("Impossivel calcular\n");
+    }
+    else
+    {
+        printf("R1 = %.5lf\n", (-B + sqrt(B*B - 4*A*C)) / (2*A));
+        printf("R2 = %.5lf\n", (-B - sqrt(B*B - 4*A*C)) / (2*A));
+    }
+
+    return 0;
+}
